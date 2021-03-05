6815bc96adb14709ca3ed377c64d124a1751c692
[ve1l] <[leve1l@yandex.ru]>
2021-03-05 18:06:16 +0300

    ES6

---
+++ lesson-2/js/main.js
@@ -0,0 +1,41 @@
+class productsList {
+  constructor(container = '.products') {
+    this.container = container
+    this.goods = []
+    this._fetchProducts()
+  }
+  _fetchProducts() {
+    this.goods = [
+      { id: 1, title: 'Notebook', price: 2000 },
+      { id: 2, title: 'Mouse', price: 20 },
+      { id: 3, title: 'Keyboard', price: 200 },
+      { id: 4, title: 'Gamepad', price: 300 },
+    ]
+  }
+  render() {
+    const block = document.querySelector(this.container)
+    for (let product of this.goods) {
+      const productObj = new ProductItem(product)
+      block.insertAdjacentHTML('beforeend', productObj.render())
+    }
+  }
+}
+class ProductItem {
+  constructor(product) {
+    this.title = product.title
+    this.price = product.price
+    this.id = product.id
+  }
+  render() {
+    return `<div class="product-item">
+    <img class="products-img"src="img/${this.title}.png" alt="${this.title}"/>
+    <h3 class="text-h3">${this.title}</h3>
+    <p class="price">${this.price}</p>
+    <button class="buy-btn">Купить</button>
+    </div>
+    <div class="product-sum"></div>`
+  }
+}
+
+let list = new productsList()
+list.render()
