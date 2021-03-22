4954a35575ef2de29666e202196b1f687e94a4bc
[ve1l] <[leve1l@yandex.ru]>
2021-03-23 01:35:32 +0300

    HW 6 completed

---
+++ lesson-6/js/ErrorData.js
@@ -0,0 +1,5 @@
+Vue.component('error-data', {
+  template: `<div class="cart-item">
+      <h3 class="text-h3">Сервер недоступен</h3>
+    </div>`,
+})
---
+++ lesson-6/js/ProductComponent.js
@@ -0,0 +1,26 @@
+Vue.component('products', {
+  props: ['products', 'img'],
+  template: `<div class="products">
+               <product v-for="item of products" 
+               :key="item.id_product" 
+               :img="img"
+               :product="item"></product>
+              </div>`,
+})
+Vue.component('product', {
+  props: ['product', 'img'],
+  template: `
+  <div class="products">
+  <div class="product-item">
+    <img :src="img" alt="Some img">
+    <div class="desc">
+      <h3 class="text-h3">{{product.product_name}}</h3>
+      <p class="price">{{product.price}} руб.</p>
+      <button class="buy-btn" @click="$parent.$emit('add-product', product)">
+        Купить
+      </button>
+    </div>
+  </div>
+</div>
+   `,
+})
